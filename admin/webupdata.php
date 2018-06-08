290b788ab965e1b0c11a36954cc98d6bd099654e
2701150216 <37213175+2701150216@users.noreply.github.com>
2018-06-08 13:18:10 +0800

    Update webupdata.php

--- admin/webupdata.php
+++ admin/webupdata.php
@@ -8,7 +8,7 @@ if ($_SESSION['admin'] == "OK") {
     $exec = "select * from webinfo";
     $exec = "update webinfo set webname ='" . $_POST['post_name'] . "',webmail = '" . $_POST['post_mail'] . "',webqq = '" . $_POST['post_qq'] . "',webstat = '" . $_POST['post_stat'] . "'"; //修改站点信息
     $result = mysql_query($exec);
-    header("location:successful.php"); //修改成功
+    header("location:successful.php"); 
 } else {
     header("location:login.php");
 }
